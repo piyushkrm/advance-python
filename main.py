53cfae749b0ff7339bfd5f3e11a6727c40789f48
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-26 10:10:58 +0530

    This is walrus operator

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-lis = ["Saloni", "Subham", "Sona", "Sumit", "Suraj" , "Shriyanka"]
-for l in lis : 
-    print(l)
+# WALRUS OPERATOR
+# The walrus operator ( := ), introduced in Python 3.8, allows you to assign values to variables as part of an expression. This operator, named for its resemblance to the eyes and tusks of a walrus, is officially called the "assignment expression."
+
+if n := len(["pihu", "sona", "piyush", "saloni", "sonam", "abhinav", "palak" ,"daisy"]) > 2:
+    print(f"List is to long ({n} elements, expected  <= 3)")
+
