dd7f1a0a5abf327fa525d390767bede0419bb6ab
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-26 11:03:14 +0530

    merge and update dictionary

---
+++ 05_merge _update_dictionary.py
@@ -0,0 +1,24 @@
+# # Dictionary Merge and Update
+
+# dict1 = {
+#     "a" : 18,
+#     "b" : 45
+# }
+
+# dict2 = {
+#     "c" : 7,
+#     "d" : 10
+# }
+
+# merged = dict1 | dict2
+# print(merged)
+
+
+'''
+You can now use miltiple context managers in a single 'with' statement more clearly using the parenthesised contect manager
+'''
+
+with (
+    open('file1.txt') as f1,
+    open('file2.txt') as f2
+) :
