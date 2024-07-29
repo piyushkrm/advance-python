ab5fe454a30700d9ebda1f42a56b410152cd7f05
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-29 10:39:14 +0530

    import __name__

--- 10_main.py
+++ 10_main.py
@@ -1,3 +1,4 @@
 from module import myFunc
 
+myFunc()
 # if run import file then print "module"
--- module.py
+++ module.py
@@ -1,7 +1,12 @@
 def myFunc() :
     print("Hello world !")
 
-myFunc()
-print(__name__)
 
-# if you run the module file then printing the "__main__"
+
+if __name__ == "__main__":
+    # if the code is directly executed by running the file its present in
+    print("We are directly running this code")
+    myFunc()
+    print(__name__)
+else :
+    print("This code is executed by other programmer not the own programmer")
