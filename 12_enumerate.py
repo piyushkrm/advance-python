28a88d451af8389c634ff0a0f07da93a8034686a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-29 10:48:46 +0530

    enumerate

---
+++ 12_enumerate.py
@@ -0,0 +1,12 @@
+li = [4, 45, 40, 78, 89]
+
+# index = 0
+# for i in li :
+#     print(f"The item number at index {index} is {i}")
+#     index += 1
+
+
+
+#    This cam be si,plified using enumerate function
+for index, item in enumerate(li) :
+    print(f"The item number at index {index} is {item}")
