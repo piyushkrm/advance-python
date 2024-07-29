670335cc2a8bdb01c13406cf7e59339333448750
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-29 10:20:06 +0530

    try with else

---
+++ 08_try_else.py
@@ -0,0 +1,11 @@
+# if try block are successfully run then else block are executed
+
+try :
+    a = int(input("Enter a number : "))
+
+except Exception as e :
+    print(e)
+
+else :
+    print("Thanku for entering correct number !")
+
