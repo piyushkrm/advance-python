805e00db18515447bd1cff49bf04402e7fc190d0
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-31 11:09:27 +0530

    join method

---
+++ Chapter 13/03_join.py
@@ -0,0 +1,9 @@
+# JOIN METHOD (STRING)
+
+# Create a string from iterable objects
+
+ls = ["Piyush", "Abhinav", "Aayush", "Palak", "Megha", "Harsh", "Prasun" ,"Daisy"]
+
+final = " :: ".join(ls)
+
+print(final)
