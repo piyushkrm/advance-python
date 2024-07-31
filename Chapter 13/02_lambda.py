998ae5658166b0d6105d3e495bf5f3f550354a4d
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-31 10:32:26 +0530

    Lambda keyword

---
+++ Chapter 13/02_lambda.py
@@ -0,0 +1,11 @@
+# LAMBDA functions:- Function create usinf an expression using "lambda" keyword
+
+def square(n) :
+    return n*n
+
+# print(square(10))
+
+#   Same problem solved by lambda keyword
+
+squar = lambda z : z*z
+print(squar(5))
