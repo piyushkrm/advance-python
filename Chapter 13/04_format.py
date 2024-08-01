196905c9806d88c0632e56a195e1d43524b97251
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-01 10:36:00 +0530

    format

---
+++ Chapter 13/04_format.py
@@ -0,0 +1,13 @@
+
+a = "{} is a good {}".format("piyush", "student")
+
+print(a)
+
+b = "{0} is a good {1}".format("piyush", "student")
+
+print(b)
+
+
+c = "{1} is a good {0}".format("piyush", "student")
+
+print(c)
