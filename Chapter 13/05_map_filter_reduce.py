c0175a869a9e26253f8af32de617cac0437e9955
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-01 10:43:19 +0530

    filter

--- Chapter 13/05_map_filter_reduce.py
+++ Chapter 13/05_map_filter_reduce.py
@@ -1,3 +1,5 @@
+#  Map Example
+
 ls = [6, 2, 3, 4, 5]
 
 square = lambda x : x*x
@@ -6,3 +8,16 @@ sprList = map(square, ls)
 
 for i in sprList:
     print(i)
+
+
+
+#  Filter Example
+lis = [10, 15, 20, 18, 13, 28]
+
+def even(n) :
+    if n % 2 == 0:
+        return True
+    return False
+
+onlyEven = filter(even, lis)
+print(list(onlyEven))
